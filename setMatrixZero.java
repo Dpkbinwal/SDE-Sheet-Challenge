3c38798c700fcab8b0ac4c2b9c9092324c09296f
Pradeep Binwal <Pradeepbinwal1@gmail.com>
2023-06-05 12:44:04 +0530

    first code

--- setMatrixZero.java
+++ setMatrixZero.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.util.* ;
 class setMatrixZero{
     public static void main(String[] args){
-        int matrix[][]={{1,0,6},{1,1,1},{1,1,3}};
+        int matrix[][]={{1,0,1},{1,1,1},{1,1,3},{2,3,0}};
         Solution s= new Solution();
         s.setZeros(matrix);
        for(int i=0;i<matrix.length;i++){
         for(int j=0;j<matrix[0].length;j++){
-            System.out.print(matrix[i][j]+" " );
+            System.out.print(matrix[i][j]+"" );
         }
         System.out.println();
        }
